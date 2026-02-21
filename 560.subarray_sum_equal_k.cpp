9c74f096b1ead9fb0d045d800051163a397d3a9c
2Gcoder <gauravgupta.8.a@gmail.com>
2026-02-22 00:45:44 +0530

    prefix Sum

---
+++ 560.subarray_sum_equal_k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+
+        mpp[0]=1; // in map key'0' pe '1' assign hoga.
+        int presum=0;// from index 0 to i tak value ka sum.
+     int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            presum += nums[i];
+            int remove = presum-k;
+            count+=mpp[remove];
+            mpp[presum]+=1;
+
+        }
+
+        return count;
+    }
+};
