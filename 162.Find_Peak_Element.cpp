6f57180d8f458bce73403ef8f1cc23b957d1f492
2Gcoder <gauravgupta.8.a@gmail.com>
2026-03-10 18:57:29 +0530

    Same binary approach to find peak when only one peak element is given

--- 162.Find_Peak_Element.cpp
+++ 162.Find_Peak_Element.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int i,j;
+        int st=1,end = nums.size()-2;
         int n= nums.size();
-
-        //Edge case for size of array 
         if(n==1 || (n==2 && nums[0] > nums[1]))
         return 0;
 
         if(n==2 && nums[0] < nums[1])
         return 1;
 
-        for(i=1;i<n-1;i++)
-        {
-            if(nums[i] > nums[i-1]  && nums[i] > nums[i+1])
-                return i ;
-        }
 
-// Edge case at first and last element 
+        // Edge case at first and last element 
        if(nums[0] > nums[1]) // first element is graeter then next then it is peak 
        return 0;
 
        if(nums[n-1] > nums[n-2] ) // last element is greater then second last than itt is peak
        return n-1;
+ 
+       while(st <= end) // Binary search , same approach as to find when single peak  element is given  
+       {
+       int  mid =(end +st)/2;
+
+        if(nums[mid-1] < nums[mid]  && nums[mid] > nums[mid+1] ) return mid;
+
+        else if(nums[mid] > nums[mid-1]) st = mid +1;
+
+        else end = mid - 1;
+       }
 
 
     
-return i;
+return -1;
 
     }
 };
