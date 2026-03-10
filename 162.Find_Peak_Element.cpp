b68ba71cd6333ccf1a63645bf7c53215b147c860
2Gcoder <gauravgupta.8.a@gmail.com>
2026-03-10 17:54:23 +0530

    compare with next and previous element and use edge case for first and last

---
+++ 162.Find_Peak_Element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int i,j;
+        int n= nums.size();
+        if(n==1 || (n==2 && nums[0] > nums[1]))
+        return 0;
+
+        if(n==2 && nums[0] < nums[1])
+        return 1;
+
+        for(i=1;i<n-1;i++)
+        {
+            if(nums[i] > nums[i-1]  && nums[i] > nums[i+1])
+                return i ;
+        }
+
+// Edge case at first and last element 
+       if(nums[0] > nums[1]) // first element is graeter then next then it is peak 
+       return 0;
+
+       if(nums[n-1] > nums[n-2] ) // last element is greater then second last than itt is peak
+       return n-1;
+
+
+    
+return i;
+
+    }
+};
